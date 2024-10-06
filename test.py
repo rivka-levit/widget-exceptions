9cefd1f718281c6c95217ac7aed0436c4ad094a1
rivka-yuliia <juliacreadora@gmail.com>
2024-10-06 11:40:53 +0300

    test log_console with subclasses

--- test.py
+++ test.py
@@ -7,13 +7,54 @@ import traceback
 
 from io import StringIO
 from contextlib import redirect_stdout
+from sys import exception
 
 from unittest import TestCase
-from exceptions import WidgetException
+from exceptions import (WidgetException,
+                        SupplierException,
+                        NotManufacturedError,
+                        ProductionDelayedError,
+                        ShippingDelayedError,
+                        CheckoutException,
+                        InventoryException,
+                        OutOfStockError,
+                        PricingException,
+                        InvalidCouponCodeError,
+                        NoStackCouponError)
 
 
 class TestWidgetExceptions(TestCase):
     """Test for WidgetException class and the children."""
+
+    def setUp(self):
+        def check_log_console(exc: type) -> None:
+            with (redirect_stdout(StringIO()) as logged_out):
+                try:
+                    raise exc()
+                except exc as e:
+                    tb = ''.join(traceback.format_exception(e))
+                    e.log_console()
+                    output = logged_out.getvalue()
+
+                    self.assertIn(e.__class__.__name__, output)
+                    self.assertIn(str(e.http_status), output)
+                    self.assertIn(e.internal_error_msg, output)
+                    self.assertIn(tb, output)
+
+        self.check_log_console = check_log_console
+        self.exceptions = [
+            WidgetException,
+            SupplierException,
+            NotManufacturedError,
+            ProductionDelayedError,
+            ShippingDelayedError,
+            CheckoutException,
+            InventoryException,
+            OutOfStockError,
+            PricingException,
+            InvalidCouponCodeError,
+            NoStackCouponError
+        ]
 
     def test_to_json_valid_json_object(self):
         """Test to_json method return a valid json object."""
@@ -28,17 +69,12 @@ class TestWidgetExceptions(TestCase):
         self.assertIn('message', returned_value)
 
     def test_log_console_output_to_console(self):
-        """Test output is realised to the console."""
+        """Test output is realised to the console for base class."""
 
-        with (redirect_stdout(StringIO()) as logged_out):
-            try:
-                raise WidgetException()
-            except WidgetException as e:
-                tb = ''.join(traceback.format_exception(e))
-                e.log_console()
-                output = logged_out.getvalue()
+        self.check_log_console(WidgetException)
 
-                self.assertIn(e.__class__.__name__, output)
-                self.assertIn(str(e.http_status), output)
-                self.assertIn(e.internal_error_msg, output)
-                self.assertIn(tb, output)
+    def test_log_console_subclasses(self):
+        """Test log_console works correctly with subclasses."""
+
+        for ex in self.exceptions:
+            self.check_log_console(ex)
